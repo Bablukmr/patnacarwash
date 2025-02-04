27da5c762f3258b52f47aece722899d51b92f635
Bablukmr <bablukmr800@gmail.com>
2025-02-05 01:58:54 +0530

    first

--- resources/views/student/login.blade.php
+++ resources/views/student/login.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin Login</title>
 
-    <!-- Google Font: Source Sans Pro -->
+    <!--1 Google Font: Source Sans Pro -->
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
@@ -16,6 +16,7 @@
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('admincss/dist/css/adminlte.min.css') }}">
 </head>
+
 
 <body class="hold-transition login-page">
     <div class="login-box">
